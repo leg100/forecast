4370a3d49d187757f545878d608f40eec6202798
Louis Garman <louisgarman@gmail.com>
2015-10-11 21:45:09 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "github.com/codegangsta/cli"
+  "github.com/aws/aws-sdk-go/service/cloudformation"
+)
+
+func main() {
+  app := cli.NewApp()
+  app.Name = "forecast"
+  app.Usage = "cloudformation dry-run"
+  app.Action = func(c *cli.Context) {
+    svc := cloudformation.New(nil)
+    resp, err := svc.ListStacks(nil)
+    if err != nil {
+      println(err.Error())
+      return
+    }
+
+    fmt.Println(resp)
+  }
+
+  app.Run(os.Args)
+}
