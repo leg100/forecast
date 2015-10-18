7938fa27dbf9c298b73f84f52d5763fedef36f6d
Louis Garman <louisgarman@gmail.com>
2015-10-18 20:47:38 +0100

    Subcommand to get template

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
   "os"
   "fmt"
+
   "github.com/codegangsta/cli"
+  "github.com/aws/aws-sdk-go/aws"
   "github.com/aws/aws-sdk-go/service/cloudformation"
 )
 
@@ -21,11 +23,28 @@ func main() {
         svc := cloudformation.New(nil)
         resp, err := svc.ListStacks(nil)
         if err != nil {
-          println(err.Error())
+          fmt.Println(err.Error())
           return
         }
 
         fmt.Println(resp)
+      },
+    },
+    {
+      Name:      "get",
+      Aliases:     []string{"g"},
+      Usage:     "get stack",
+      Action: func(c *cli.Context) {
+        stack_name := c.Args()[0]
+        svc := cloudformation.New(nil)
+        params := &cloudformation.GetTemplateInput{StackName: aws.String(stack_name)}
+        resp, err := svc.GetTemplate(params)
+        if err != nil {
+          fmt.Println(err.Error())
+          return
+        }
+
+        fmt.Println(*resp.TemplateBody)
       },
     },
   }
