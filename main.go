460a1cf5cc725112cd16d84a0f87f8d454173824
Louis Garman <louisgarman@gmail.com>
2015-11-22 15:59:26 +0000

    Remove validation function

--- main.go
+++ main.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-  "os"
-  "fmt"
+	"fmt"
+	"os"
 
-  "github.com/codegangsta/cli"
-  "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/codegangsta/cli"
 )
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "forecast"
-  app.Usage = "cloudformation dry-run"
+	app := cli.NewApp()
+	app.Name = "forecast"
+	app.Usage = "cloudformation dry-run"
 
-  app.Commands = []cli.Command{
-    {
-      Name:      "list",
-      Aliases:     []string{"l"},
-      Usage:     "list stacks",
-      Action: func(c *cli.Context) {
-        svc := cloudformation.New(nil)
-        resp, err := svc.ListStacks(nil)
-        if err != nil {
-          fmt.Println(err.Error())
-          return
-        }
+	app.Commands = []cli.Command{
+		{
+			Name:    "list",
+			Aliases: []string{"l"},
+			Usage:   "list stacks",
+			Action: func(c *cli.Context) {
+				svc := cloudformation.New(nil)
+				resp, err := svc.ListStacks(nil)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
 
-        fmt.Println(resp)
-      },
-    },
-    {
-      Name:      "get",
-      Aliases:     []string{"g"},
-      Usage:     "get stack",
-      Action: func(c *cli.Context) {
-        stack_name := c.Args()[0]
-        svc := cloudformation.New(nil)
-        params := &cloudformation.GetTemplateInput{StackName: aws.String(stack_name)}
-        resp, err := svc.GetTemplate(params)
-        if err != nil {
-          fmt.Println(err.Error())
-          return
-        }
+				fmt.Println(resp)
+			},
+		},
+		{
+			Name:    "get",
+			Aliases: []string{"g"},
+			Usage:   "get stack",
+			Action: func(c *cli.Context) {
+				stack_name := c.Args()[0]
+				svc := cloudformation.New(nil)
+				params := &cloudformation.GetTemplateInput{StackName: aws.String(stack_name)}
+				resp, err := svc.GetTemplate(params)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
 
-        fmt.Println(*resp.TemplateBody)
-      },
-    },
-  }
+				fmt.Println(*resp.TemplateBody)
+			},
+		},
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }
